d9aaaa986b53d754c69c2a613cc1d8c2f6244265
Tom Blacknell <tom.websites@fastmail.co.uk>
2020-07-30 17:14:52 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './components/app';
+// import './assets/styles/main.css'
+
+let root = document.createElement('div');
+root.id = 'root';
+document.body.appendChild(root);
+
+render(<App />, document.getElementById('root'));
