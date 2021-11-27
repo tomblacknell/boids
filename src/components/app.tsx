a697ab9908743bbbc4e96688adec05b8572c57b5
Tom Blacknell <tom.websites@fastmail.co.uk>
2021-11-27 20:00:18 +0000

    mobile error page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,9 +3,17 @@ import Boids from './boids';
 
 const App = ({ }) => {
 
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
   return (
     <div className="app">
-      <Boids />
+      {isMobile ? (
+        <div className="mobileError">
+          <h1>Sorry, this doesn't work on mobile :(</h1>
+          <p>(project runs on WebGL)</p>
+          <a href="https://tom-b.dev"><button>Back to tom-b.dev</button></a>
+        </div>
+      ) : <Boids />}
     </div>
   );
 };
